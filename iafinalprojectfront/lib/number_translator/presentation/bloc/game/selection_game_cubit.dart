cc39b3d329b0930abf3c79c48f8224adc83a08d5
Victor Palmero <victor.palmero@undostres.com.mx>
2024-09-29 09:35:03 -0400

    feat: some implementations in the cubit and states

--- iafinalprojectfront/lib/number_translator/presentation/bloc/game/selection_game_cubit.dart
+++ iafinalprojectfront/lib/number_translator/presentation/bloc/game/selection_game_cubit.dart
@@ -1,37 +1,69 @@
+import 'dart:math';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:iafinalprojectfront/config/service_locator/service_locator.dart';
+import 'package:iafinalprojectfront/number_translator/domain/entities/consult_entity.dart';
+import 'package:iafinalprojectfront/number_translator/domain/use_cases/auth/auth_service.dart';
+import 'package:iafinalprojectfront/number_translator/domain/use_cases/score/score_service.dart';
+
+import '../../../domain/entities/user_score_entity.dart';
+import '../../../domain/use_cases/number_translator/number_translator_service.dart';
 
 part 'selection_game_state.dart';
 
 class SelectionGameCubit extends Cubit<SelectionGameState> {
   SelectionGameCubit() : super(SelectionGameInitial());
-  String currentWord = '';
-  int currentRound = 0;
-  int currentPoints = 0;
+  final TextEditingController responseTextController = TextEditingController();
 
-  void startGame(){
-    //TODO 9/25/24 palmerodev : llamar para obtener los numeros y traducciones
-
-    //TODO 9/25/24 palmerodev : guardar en el estado del cubit
-
-    //TODO 9/25/24 palmerodev : elegir la palabra a esconder
-
-    //TODO 9/25/24 palmerodev : iniciar el round
+  Future<void> startGame() async {
+    var number = Random().nextInt(1000000000);
+    var response = await serviceLocator.get<NumberTranslatorService>().makeTranslate(request: ConsultEntity(number: '$number'), isFromDigit: true);
+    var words = response.data.hashResponse.split(' ');
+    var currentRound = state is SelectionGameInitial ? 1 : state is SelectionGameInProgress ? (state as SelectionGameInProgress).round + 1 : 1;
+    var currentPoints = state is SelectionGameInitial ? 0 : state is SelectionGameInProgress ? (state as SelectionGameInProgress).points : 0;
+    emit(SelectionGameInProgress(
+      word: words[Random().nextInt(words.length)],
+      round: currentRound,
+      points: currentPoints,
+      healthPercentage: 1.0,
+    ));
   }
 
-  void finishCurrentRound(String wordFromUser){
-    //TODO 9/25/24 palmerodev : buscar del estado, la palabra que puso el user y comparar con la palabra escogida
-
-    //TODO 9/25/24 palmerodev : si son iguales, pasar al siguiente round ( startGame() )
-
-    //TODO 9/25/24 palmerodev : calcular la puntuacion
-
-    //TODO 9/25/24 palmerodev : si no son iguales, se acaba y se llama a finishGame()
+  void finishCurrentRound() {
+    if (state is! SelectionGameInProgress) return;
+    var currentState = state as SelectionGameInProgress;
+    var currentUserWord = responseTextController.text;
+    if (currentUserWord == currentState.word) {
+      emit(currentState.copyWith(
+        round: currentState.round + 1,
+        points: (currentState.points + currentState.healthPercentage * currentState.round) as int,
+        healthPercentage: currentState.healthPercentage,
+      ));
+      return;
+    }
+    if (currentState.healthPercentage - currentState.round * 0.05 <= 0) {
+      emit(SelectionGameFinished(points: currentState.points));
+      return;
+    }
+    emit(currentState.copyWith(healthPercentage: currentState.healthPercentage - currentState.round * 0.05));
   }
 
-  void finishGame(){
-    //TODO 9/25/24 palmerodev : calcular la puntuacion total
+  void finishGame() {
+    if (state is! SelectionGameInProgress) return;
+    serviceLocator.get<ScoreService>().save(
+      UserScoreEntity(
+        username: serviceLocator.get<AuthService>().get()!.username,
+        score: '${(state as SelectionGameFinished).points}',
+        date: DateTime.now().toString(),
+      ),
+    );
 
-    //TODO 9/25/24 palmerodev : emitir el estado final
+    reset();
+  }
+
+  void reset() {
+    emit(SelectionGameInitial());
   }
 }
--- iafinalprojectfront/lib/number_translator/presentation/bloc/game/selection_game_state.dart
+++ iafinalprojectfront/lib/number_translator/presentation/bloc/game/selection_game_state.dart
@@ -8,3 +8,40 @@ class SelectionGameInitial extends SelectionGameState {
   @override
   List<Object> get props => [];
 }
+
+class SelectionGameInProgress extends SelectionGameState {
+  final String word;
+  final int round;
+  final int points;
+  final double healthPercentage;
+
+  const SelectionGameInProgress({required this.word, required this.round, required this.points, required this.healthPercentage});
+
+  @override
+  List<Object> get props => [word, round, points, healthPercentage];
+
+  // copy with
+  SelectionGameInProgress copyWith({String? word, int? round, int? points, double? healthPercentage}) {
+    return SelectionGameInProgress(
+      word: word ?? this.word,
+      round: round ?? this.round,
+      points: points ?? this.points,
+      healthPercentage: healthPercentage ?? this.healthPercentage,
+    );
+  }
+}
+
+class SelectionGameFinished extends SelectionGameState {
+  final int points;
+
+  @override
+  List<Object> get props => [points];
+
+  const SelectionGameFinished({required this.points});
+
+  SelectionGameFinished copyWith({int? points}) {
+    return SelectionGameFinished(
+      points: points ?? this.points,
+    );
+  }
+}
